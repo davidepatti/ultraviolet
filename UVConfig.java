bf96908373be9706bd37d0c98437f6f104f216ab
davidepatti <xedivad@gmail.com>
2024-02-21 13:16:21 +0100

    Introduced NodeProfile class and updated NodeAttributes management
    The UVConfig has been updated with the addition of a new NodeProfile class to replace the Map data structure for better handling of node attributes and distributions. This change provides more structured and efficient management of node profiles, particularly with regards to parameters like ppm fees and channel sizes. The initialization process of node profiles has also been updated to parse properties and create sample distributions.

--- UVConfig.java
+++ UVConfig.java
@@ -1,5 +1,7 @@
+import java.awt.image.AreaAveragingScaleFilter;
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 /* a .properties file is used to initialize this class with a set of key/value pair, readable from other classes
 While some of them a hardcode into UV, user can add and define new keys/value pairs for new purposes, as they
 will be automatically loaded into the properties object and retrieved with:
@@ -7,37 +9,84 @@ getStringProperty/getIntProperty(String key)
 
 Properties that begin with "profile.", e.g., profile.PROFILENAME.SOME_KEY= value will be also copied into a "profiles"
 map, so that attribute can be grouped in different namespaces.
-For example:
-getProfileAttribute("big_nodes","max_channel_size") will be different from
-getProfileAttribute("small_nodes", "max_channel_size")
 
 Finally, when properties are in the particular form: key=a,b,c,d,e... they will be considered as multiple possible
 values of that property, accessible with:
 ArrayList<String> getMultivalProperty(String key)
 String getRandomMultivalProperty(String key)
-
-
-
-
  */
 
 public class UVConfig implements Serializable {
 
-    private final Map<String,Map<String,String>> profiles = new HashMap<>();
+    private final Map<String,NodeProfile> profiles = new HashMap<>();
     private Properties properties;
     private Random random;
 
+
+    public static class NodeProfile {
+
+        private final Map<String,String> attributes = new HashMap<>();
+        private final Map<String,int[]> distributions = new HashMap<>();
+        private final String name;
+
+        public NodeProfile(String profileName) {
+            this.name = profileName;
+        }
+
+        public String getAttribute(String key) {
+            try {
+                if (!attributes.containsKey(key)){
+                    throw new IllegalArgumentException("The provided key does not exist in the attributes map:"+key);
+                }
+            } catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+                return null; // return null or a default value
+            }
+            return attributes.get(key);
+        }
+
+        public int getIntAttribute(String key) {
+            return Integer.parseInt(getAttribute(key));
+        }
+        public double getDoubleAttribute(String key) {
+            return Double.parseDouble(getAttribute(key));
+        }
+
+        public void addAttribute(String key, String value) {
+            attributes.putIfAbsent(key, value);
+        }
+
+        public int getRandomSample(String key) {
+            try {
+                if (distributions.containsKey(key)) {
+                    var samples = distributions.get(key);
+                    return samples[ThreadLocalRandom.current().nextInt(0, samples.length)];
+                }
+                else  throw new RuntimeException("Missing distribution key: " + key);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return 0;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
     @Serial
     private static final long serialVersionUID = 120678L;
-
     private boolean initialized = false;
-
     public boolean isInitialized() {
         return initialized;
     }
 
-    public Map<String, Map<String, String>> getProfiles() {
+    public Map<String, NodeProfile> getNodeProfiles() {
         return profiles;
+    }
+
+    public NodeProfile getNodeProfile(String profile_name) {
+        return profiles.get(profile_name);
     }
 
     public void setDefaults() {
@@ -98,23 +147,50 @@ public class UVConfig implements Serializable {
             properties.load(new FileReader(config_file));
             initialized = true;
 
+            // Setting node profiles
             for (String propertyName : properties.stringPropertyNames()) {
-                var value = properties.getProperty(propertyName);
                 if (propertyName.startsWith("profile.")) {
                     // Parse the property name into profile name and attribute
                     String[] parts = propertyName.split("\\.");
                     String profileName = parts[1];
                     String attribute = parts[2];
 
-                    profiles.putIfAbsent(profileName,new HashMap<>());
-                    profiles.get(profileName).put(attribute,value);
+                    profiles.putIfAbsent(profileName,new NodeProfile(profileName));
+                    var value = properties.getProperty(propertyName);
+                    var profile = profiles.get(profileName);
+                    profile.addAttribute(attribute,value);
                 }
             }
 
-            // put the name of each profile as attribute of the profile itself
-            for (String name:profiles.keySet()) {
-                profiles.get(name).put("name",name);
-                //System.out.println("Loaded profile: "+name);
+            // parse profiles to create samples distributions
+            for (var profile: profiles.values()) {
+                final int max_ch_size = profile.getIntAttribute("max_channel_size");
+                final int min_ch_size = profile.getIntAttribute("min_channel_size");
+                final int median_ch_size = profile.getIntAttribute("median_channel_size");
+                final int mean_ch_size = profile.getIntAttribute("mean_channel_size");
+                final int max_ppm_fee = profile.getIntAttribute("max_ppm_fee");
+                final int min_ppm_fee = profile.getIntAttribute("min_ppm_fee");
+                final int median_ppm_fee = profile.getIntAttribute("median_ppm_fee");
+                final int mean_ppm_fee = profile.getIntAttribute("mean_ppm_fee");
+
+                profile.distributions.put("channel_sizes",DistributionGenerator.generateIntSamples(100,min_ch_size,max_ch_size,median_ch_size,mean_ch_size));
+                profile.distributions.put("ppm_fees",DistributionGenerator.generateIntSamples(100,min_ppm_fee,max_ppm_fee,median_ppm_fee,mean_ppm_fee));
+
+                Arrays.sort(profile.distributions.get("channel_sizes"));
+                Arrays.sort(profile.distributions.get("ppm_fees"));
+
+                System.out.println(profile.distributions.get("channel_sizes"));
+                System.out.println(profile.distributions.get("ppm_fees"));
+
+
+                int[] channelSizes = profile.distributions.get("channel_sizes");
+                int[] ppmFees = profile.distributions.get("ppm_fees");
+
+                double averageChannelSizes = Arrays.stream(channelSizes).average().getAsDouble();
+                double averagePpmFees = Arrays.stream(ppmFees).average().getAsDouble();
+
+                System.out.println("The average of channel_sizes is: " + averageChannelSizes);
+                System.out.println("The average of ppm_fees is: " + averagePpmFees);
             }
 
         } catch (FileNotFoundException e) {
@@ -160,14 +236,14 @@ public class UVConfig implements Serializable {
         return Integer.parseInt(getMultivalRandomItem(key));
     }
 
-    public synchronized Map<String,String> getRandomProfile() {
+    public synchronized NodeProfile getRandomProfile() {
         double p = random.nextDouble();
         double cumulativeProbability = 0.0;
         for (String profileName: profiles.keySet()) {
 
             if (!profileName.equals("default")) {
                 try {
-                    cumulativeProbability += Double.parseDouble(profiles.get(profileName).get("prob"));
+                    cumulativeProbability += Double.parseDouble(profiles.get(profileName).getAttribute("prob"));
                 }
                 catch (RuntimeException e) {
                     System.out.println("Wrong get request!");
@@ -183,7 +259,7 @@ public class UVConfig implements Serializable {
     }
 
     public String getProfileAttribute(String profile, String attribute) {
-        return profiles.get(profile).get(attribute);
+        return profiles.get(profile).getAttribute(attribute);
     }
 
     public String getStringProperty(String parameter) {
