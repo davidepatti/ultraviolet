51523aae5fb0ebbdde0213f11dcc43972bb7cd3e
davidepatti <xedivad@gmail.com>
2024-04-17 18:20:17 +0200

    Refactor UVChannel for better balance calculation and output formatting
    Changes on the UVChannel.java include an updated calculation for node balance by subtracting the pending amount. The string format for printing channel and node information has also been refined for more readability. Furthermore, redundant methods have been eliminated, and the way channel ID is constructed has been updated.

--- src/UVChannel.java
+++ src/UVChannel.java
@@ -48,28 +48,28 @@ public class UVChannel implements LNChannel, Serializable, Comparable<LNChannel>
         this.reserve = channelReserveSatoshis;
 
         if (init_direction) {
-            node1 = new NodeData(pub1, null, fundingSatoshis, 0);
+            node1 = new NodeData(pub1, null, fundingSatoshis-pushMsat, 0);
             node2 = new NodeData(pub2, null, pushMsat, 0);
         }
         else {
             node1 = new NodeData(pub1, null, pushMsat, 0);
-            node2 = new NodeData(pub2, null, fundingSatoshis, 0);
+            node2 = new NodeData(pub2, null, fundingSatoshis-pushMsat, 0);
         }
 
     }
     // constructor only fill the "proposal" for the channel
     public UVChannel(String initiator, String peer, int fundingSatoshis, int channelReserveSatoshis, int pushMsat) {
         if (initiator.compareTo(peer)<0) {
-            node1 = new NodeData(initiator, null, fundingSatoshis, 0);
+            node1 = new NodeData(initiator, null, fundingSatoshis-pushMsat, 0);
             node2 = new NodeData(peer, null, pushMsat, 0);
             init_direction = true;
         }
         else {
-            node2 = new NodeData(initiator, null, fundingSatoshis, 0);
+            node2 = new NodeData(initiator, null, fundingSatoshis-pushMsat, 0);
             node1 = new NodeData(peer, null, pushMsat, 0);
             init_direction = false;
         }
-        this.channel_id = "ch_"+node1.pubkey +"-"+node2.pubkey;
+        this.channel_id = "id_"+node1.pubkey +"_"+node2.pubkey;
         this.reserve = channelReserveSatoshis;
     }
 
@@ -136,6 +136,14 @@ public class UVChannel implements LNChannel, Serializable, Comparable<LNChannel>
         var node = resolveNode(node_id);
         return node.balance;
     }
+    /**
+     * Evaluate the current liquidity on channel peer side, considering the reserve to be mantained and the pending transactions
+     * @return the amount of sats that could actually be received from peer
+     */
+    public synchronized int getLiquidity(String pubkey) {
+        var node = resolveNode(pubkey);
+        return node.balance - getReserve() - node.pending;
+    }
 
 
     public Policy getPolicy(String pubkey) {
@@ -144,30 +152,12 @@ public class UVChannel implements LNChannel, Serializable, Comparable<LNChannel>
         return node.policy;
     }
 
-
     public synchronized int getLastCommitNumber() {
         return this.commitNumber;
     }
 
     public synchronized int getReserve() {
         return reserve;
-    }
-
-    public synchronized int getMinLiquidity() {
-        return getReserve();
-    }
-
-    public synchronized int getMaxLiquidity() {
-        return getCapacity()-getReserve();
-    }
-
-    /**
-     * Evaluate the current liquidity on channel peer side, considering the reserve to be mantained and the pending transactions
-     * @return the amount of sats that could actually be received from peer
-     */
-    public synchronized int getLiquidity(String pubkey) {
-        var node = resolveNode(pubkey);
-        return node.balance - getReserve() - node.pending;
     }
 
     public synchronized int getNextHTLCid() {
@@ -179,7 +169,7 @@ public class UVChannel implements LNChannel, Serializable, Comparable<LNChannel>
     public synchronized void newCommitment(int node1Balance, int node2Balance) {
 
         if (node1Balance+node2Balance != this.getCapacity()) {
-            throw new IllegalArgumentException("Ch["+this.getId()+"] Wrong balances in commitment:"+node1Balance+"+"+node2Balance+"!= "+getCapacity());
+            throw new IllegalArgumentException("Channel "+this.getId()+": Wrong balances in commitment:"+node1Balance+"+"+node2Balance+"!= "+getCapacity());
         }
         node1.balance = node1Balance;
         node2.balance = node2Balance;
@@ -188,20 +178,12 @@ public class UVChannel implements LNChannel, Serializable, Comparable<LNChannel>
 
     @Override
     public String toString() {
+        StringBuilder s =  new StringBuilder(String.format("%-15s %-6s %-6s %-10d %-10d %-12s %-12s", channel_id, node1.pubkey, node2.pubkey, node1.balance, node2.balance,node1.policy, node2.policy));
 
-        StringBuilder p1 = new StringBuilder("(");
-        p1.append(channel_id).append(") ").append("n1:").append(node1.pubkey);
-        p1.append(" n2:").append(node2.pubkey).append(", [").append(node1.balance);
+        if (node1.pending!=0) s.append(" (pending n1:").append(node1.pending).append(")");
+        if (node2.pending!=0) s.append(" (pending n2:").append(node2.pending).append(")");
 
-        if (node1.pending!=0) p1.append("(pending ").append(node1.pending).append(")");
-
-        p1.append(",").append(node2.balance);
-        if (node2.pending!=0) p1.append("(pending ").append(node2.pending).append(")");
-
-        p1.append("]").append(node1.policy).append(node2.policy);
-        p1.append(", commit:").append(commitNumber);
-
-        return p1.toString();
+        return s.toString();
     }
 
     /**
